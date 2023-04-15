1c7fbee665a25da4a4e8c8b1978f62395f694f1b
Joe Yates <joe.g.yates@gmail.com>
2023-04-15 11:40:21 +0200

    Replace static home page with a live one

--- lib/caveatica_controller_web/controllers/page_controller.ex
+++ lib/caveatica_controller_web/controllers/page_controller.ex
@@ -1,11 +1,3 @@
 defmodule CaveaticaControllerWeb.PageController do
   use CaveaticaControllerWeb, :controller
-
-  @image_path Application.compile_env(:caveatica_controller, :webcam_image_path)
-
-  def home(conn, _params) do
-    conn
-    |> assign(:image_path, @image_path)
-    |> render(:home, layout: false)
-  end
 end
