3dd7a67970cf0416caf152f5807c4abe9c84872d
Joe Yates <joe.g.yates@gmail.com>
2023-04-15 12:08:43 +0200

    Add timezone database

--- mix.exs
+++ mix.exs
@@ -36,6 +36,7 @@ defmodule CaveaticaController.MixProject do
       {:phoenix_live_dashboard, "~> 0.7.2"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
+      {:tzdata, "~> 1.1"},
       # Assets
       {:esbuild, "~> 0.5", runtime: Mix.env() == :dev},
       {:tailwind, "~> 0.1.8", runtime: Mix.env() == :dev}
