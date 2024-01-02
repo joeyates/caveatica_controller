857680a5911fb196502dfce1404fe359dc838319
Joe Yates <joe.g.yates@gmail.com>
2024-01-02 15:45:06 +0100

    Receive image uploads via websocket

--- lib/caveatica_controller_web/channels/control_channel.ex
+++ lib/caveatica_controller_web/channels/control_channel.ex
@@ -3,6 +3,8 @@ defmodule CaveaticaControllerWeb.ControlChannel do
 
   require Logger
 
+  alias Phoenix.PubSub
+
   def join("control", _message, socket) do
     Logger.info("Control channel joined")
     {:ok, %{ok: "good"}, socket}
@@ -11,6 +13,21 @@ defmodule CaveaticaControllerWeb.ControlChannel do
   def handle_in("get_metrics", _params, socket) do
     Logger.info("Control channel get_metrics")
     {:reply, {:ok, %{result: "ok"}}, socket}
-    #{:noreply, socket}
+  end
+
+  def handle_in("upload_image", %{"binary" => encoded}, socket) do
+    binary = Base.decode64!(encoded)
+    Logger.info("Control channel upload_image, binary: #{byte_size(binary)}")
+    File.write!(original_path(), binary)
+    PubSub.broadcast(CaveaticaController.PubSub, "image_upload", {:image_upload})
+    {:noreply, socket}
+  end
+
+  defp data_path do
+    Application.fetch_env!(:caveatica_controller, :data_path)
+  end
+
+  defp original_path do
+    Path.join(data_path(), "caveatica.jpg")
   end
 end
--- lib/caveatica_controller_web/live/home_live/index.ex
+++ lib/caveatica_controller_web/live/home_live/index.ex
@@ -1,23 +1,24 @@
 defmodule CaveaticaControllerWeb.HomeLive.Index do
   use CaveaticaControllerWeb, :live_view
 
-  alias CaveaticaController.Cldr.DateTime.Relative
+  require Logger
 
-  @update_image_interval 500 # ms
-  @update_image_age_interval 1000 # ms
+  alias CaveaticaController.Cldr.DateTime.Relative
+  alias Phoenix.PubSub
+
   @server_timezone "Etc/UTC"
   @user_timezone "Europe/Rome"
   @maximum_image_dimension 320
 
   @impl true
   def mount(_params, _session, socket) do
+    PubSub.subscribe(CaveaticaController.PubSub, "image_upload")
     {
       :ok,
       socket
       |> assign(:image_path, nil)
       |> assign(:image_timestamp, nil)
-      |> process_image()
-      |> update_image_age()
+      |> assign_image_age()
     }
   end
 
@@ -43,13 +44,9 @@ defmodule CaveaticaControllerWeb.HomeLive.Index do
   end
 
   @impl true
-  def handle_info(:update_image, socket) do
+  def handle_info({:image_upload}, socket) do
+    Logger.info("HomeLive.Index handle_info image_upload")
     {:noreply, process_image(socket)}
-  end
-
-  @impl true
-  def handle_info(:update_image_age, socket) do
-    {:noreply, update_image_age(socket)}
   end
 
   @impl true
@@ -58,26 +55,16 @@ defmodule CaveaticaControllerWeb.HomeLive.Index do
   end
 
   defp process_image(socket) do
-    Process.send_after(self(), :update_image, @update_image_interval)
+    original_path = original_path()
+    original_exists = File.exists?(original_path)
 
-    original_relative_path = "./priv/static/#{static_image_path()}"
-    original_exists = File.exists?(original_relative_path)
-    have_image = if socket.assigns.image_path, do: true, else: false
-    load_initial = !have_image && original_exists
-
-    relative_lock_path = "./priv/static/#{lock_path()}"
-    lock_exists = File.exists?(relative_lock_path)
-
-    if load_initial || lock_exists do
-      timestamp = timestamp(original_relative_path)
-      converted_relative_path = "./priv/static/#{converted_path()}"
-      :ok = rotate_90(original_relative_path, converted_relative_path)
-      if lock_exists do
-        File.rm(relative_lock_path)
-      end
+    if original_exists do
+      timestamp = timestamp(original_path)
+      converted_path = converted_path()
+      :ok = rotate_90(original_path, converted_path)
       epoch = DateTime.to_unix(timestamp)
       socket
-      |> assign(:image_path, "/#{converted_path()}?time=#{epoch}")
+      |> assign(:image_path, "/converted.jpg?time=#{epoch}")
       |> assign(:image_timestamp, timestamp)
       |> assign_image_age()
     else
@@ -85,24 +72,16 @@ defmodule CaveaticaControllerWeb.HomeLive.Index do
     end
   end
 
-  defp static_image_path do
-    Application.fetch_env!(:caveatica_controller, :webcam_image_path)
+  defp data_path do
+    Application.fetch_env!(:caveatica_controller, :data_path)
   end
 
-  defp lock_path, do: String.replace(static_image_path(), ".jpg", ".lock")
+  defp original_path do
+    Path.join(data_path(), "caveatica.jpg")
+  end
 
   defp converted_path do
-    path = static_image_path()
-    parts = Path.split(path)
-    [filename | rest] = Enum.reverse(parts)
-    ["converted-#{filename}" | rest]
-    |> Enum.reverse()
-    |> Path.join()
-  end
-
-  defp update_image_age(socket) do
-    Process.send_after(self(), :update_image_age, @update_image_age_interval)
-    assign_image_age(socket)
+    Path.join(data_path(), "converted.jpg")
   end
 
   defp assign_image_age(socket) do
