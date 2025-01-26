caa6512852c04c398a401217225b52adff190538
Joe Yates <joe.g.yates@gmail.com>
2025-01-26 11:31:29 +0100

    List existing jobs

--- lib/caveatica_controller/scheduler.ex
+++ lib/caveatica_controller/scheduler.ex
@@ -7,9 +7,16 @@ defmodule CaveaticaController.Scheduler do
 
   def reset() do
     Logger.info("Resetting Open/Close times")
+    Logger.info("Before:")
+    list_jobs()
     next_close = Times.next_close!()
     next_open = Times.next_open!()
     Logger.info("next_close: #{inspect(next_close)}")
     Logger.info("next_open: #{inspect(next_open)}")
+  end
+
+  defp list_jobs() do
+    jobs = jobs()
+    Logger.info("Current Jobs: #{inspect(jobs)}")
   end
 end
