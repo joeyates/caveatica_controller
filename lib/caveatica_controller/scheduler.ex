ebee2b4a4e8a70751b8ef50f82f41ee47623bc9d
Joe Yates <joe.g.yates@gmail.com>
2025-09-27 21:34:38 +0200

    Recalculate next cron jobs after an open/close

--- lib/caveatica_controller/scheduler.ex
+++ lib/caveatica_controller/scheduler.ex
@@ -20,7 +20,9 @@ defmodule CaveaticaController.Scheduler do
     list_jobs()
     delete_existing()
     add_job(open_job())
+    add_job(reset_after_open_job())
     add_job(close_job())
+    add_job(reset_after_close_job())
     Logger.info("After:")
     list_jobs()
   end
@@ -97,6 +99,44 @@ defmodule CaveaticaController.Scheduler do
       CaveaticaControllerWeb.Endpoint.broadcast!("control", "close", %{
         "duration" => close_duration
       })
+    end)
+  end
+
+  defp reset_after_open_job() do
+    next_open = Times.next_open!()
+    after_next_open = DateTime.add(next_open, 1, :minute)
+
+    schedule = %Crontab.CronExpression{
+      hour: [after_next_open.hour],
+      minute: [after_next_open.minute]
+    }
+
+    new_job()
+    |> Quantum.Job.set_name(:reset_after_open)
+    |> Quantum.Job.set_schedule(schedule)
+    |> Quantum.Job.set_task(fn ->
+      Logger.info("Resetting after open via cron job")
+
+      CaveaticaController.Scheduler.reset()
+    end)
+  end
+
+  defp reset_after_close_job() do
+    next_close = Times.next_close!()
+    after_next_close = DateTime.add(next_close, 1, :minute)
+
+    schedule = %Crontab.CronExpression{
+      hour: [after_next_close.hour],
+      minute: [after_next_close.minute]
+    }
+
+    new_job()
+    |> Quantum.Job.set_name(:reset_after_close)
+    |> Quantum.Job.set_schedule(schedule)
+    |> Quantum.Job.set_task(fn ->
+      Logger.info("Resetting after close via cron job")
+
+      CaveaticaController.Scheduler.reset()
     end)
   end
 
